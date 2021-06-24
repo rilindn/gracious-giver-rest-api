3352e1d7963d0fbab0edba782022be1483f1cebf
Rilinda Xhaqkaj <xhaqkaj.r@gmail.com>
2021-06-25 00:32:23 +0200

    Bookmark product model-controller

---
+++ GraciousGiver-BackEnd/Models/Bookmark.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GraciousGiver_BackEnd.Models
+{
+    public class Bookmark
+    {
+        [Key]
+        public int BookmarkId { get; set; }
+        public int ProductId { get; set; }
+        public int UserId { get; set; }
+    }
+}
